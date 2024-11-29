d0f5f5019b6cb0a7d1705b60c36321c279e0d707
Nishant <nishantdobaria170@gmail.com>
2024-11-29 22:39:55 +0530

    env local db

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,8 @@ config();
 
 export const env = {
   PORT: Number(process.env.PORT) || 5000,
-  DATABASE_URL: process.env.DATABASE_URL || "",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "mongodb://localhost:27017/user-service",
   JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY || "",
   JWT_PUBLIC_KEY: process.env.JWT_PUBLIC_KEY || "",
   PASSWORD_PRIVATE_KEY: process.env.PASSWORD_PRIVATE_KEY || "",
